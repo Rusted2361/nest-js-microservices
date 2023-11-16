d47ddf3bd3fcfcd837b456386e0265de5db318ee
Rusted2361 <hamzasajid4040@gmail.com>
2023-11-16 20:34:37 +0500

    added test

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      url: 'localhost:5000',
+      package: 'hello',
+      protoPath: './hello.proto',
+    },
+  });
+  await app.listen();
 }
 bootstrap();
